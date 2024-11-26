0160e638637e5c52822ee10ed5a4476abac2cfca
swarup Goswami <goswamiswarup97@gmail.com>
2024-11-26 23:39:49 +0530

    created profile end points in the userapislice file

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -1,5 +1,6 @@
 import { apiSlice } from "./apiSlice";
-import { USERS_URL } from "../features/constants";
+import { BASE_URL, USERS_URL } from "../features/constants";
+
 
 
 export const userApislice = apiSlice.injectEndpoints({
@@ -11,9 +12,35 @@ export const userApislice = apiSlice.injectEndpoints({
                 body:data,
                 
 
+            }),
+        }),
+
+
+        logout:builder.mutation({
+            query:()=>({
+                url:`${USERS_URL}/logout`,
+                method:'POST',
+
+
+            }),
+        }),
+
+        register:builder.mutation({
+            query:data=>({
+                url:`${USERS_URL}`,
+                method:'POST',
+                body:data,
+            })
+        }),
+
+        profile:builder.mutation({
+            query:(data)=>({
+                url:`${USERS_URL}/profile`,
+                method:'post',
+                body:data
             })
         })
-    })
+    }),
 })
 
-export const {useLoginMutation}= userApislice
+export const {useLoginMutation , useLogoutMutation ,useRegisterMutation, useProfileMutation}= userApislice
